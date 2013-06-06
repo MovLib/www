75ee31be9f446503402461cb8d15eba95893459f
Richard Fussenegger <richard@fussenegger.info>
2013-06-06 11:06:04 +0200

    Removed delayed model in favor of  delayed method calls which are more flexible and faster. @todo Debug current implementation, it's not saving the messages (maybe due to affected_rows check). @todo Combine I18n into I18nModel and delete I18n because it only acts as proxy which we don't need.

--- src/MovLib/Model/I18nModel.php
+++ src/MovLib/Model/I18nModel.php
@@ -19,8 +19,8 @@ namespace MovLib\Model;
 
 use \MovLib\Exception\ErrorException;
 use \MovLib\Model\AbstractModel;
-use \MovLib\Model\DelayedModel;
 use \MovLib\Utility\DelayedLogger;
+use \MovLib\Utility\DelayedMethodCalls;
 
 /**
  * Description of I18nModel
@@ -51,11 +51,12 @@ class I18nModel extends AbstractModel {
    *   Message in desired language or <var>$message</var> if no translation exist.
    */
   public function getMessage($message, $languageCode, $options) {
-    $comment = isset($options["comment"]) ? $options["comment"] : null;
-    $oldMessage = isset($options["old_message"]) ? $options["old_message"] : null;
     try {
-      $translation = $this->query(
+      // We have to select the ID as well, otherwise we wouldn't know if the exception happened because of the message
+      // being not present in the database or if we're simply missing the translation for it.
+      return $this->query(
         "SELECT
+          `message_id`,
           COLUMN_GET(`dyn_translations`, '{$languageCode}' AS BINARY) AS `translation`
         FROM `messages`
           WHERE `message` = ?
@@ -65,31 +66,9 @@ class I18nModel extends AbstractModel {
       )[0]["translation"];
     } catch (ErrorException $e) {
       DelayedLogger::log("Could not find {$languageCode} translation for message: '{$message}'", E_NOTICE);
-      DelayedModel::stackCallback(function ($delayedCallback) use ($message, $comment, $oldMessage) {
-        //
-        // @todo How can we make this easier and DRY?
-        //
-        if ($oldMessage && $comment) {
-          $delayedCallback->update("messages", "ss", [ "message" => $message, "comment" => $comment ], [ "message" => $oldMessage ]);
-          if ($delayedCallback->getMySQLi()->affected_rows === 0) {
-            $delayedCallback->insert("messages", "ss", [ "message" => $message, "comment" => $comment ]);
-          }
-        } elseif ($oldMessage) {
-          $delayedCallback->update("messages", "s", [ "message" => $message ], [ "message" => $oldMessage ]);
-          if ($delayedCallback->getMySQLi()->affected_rows === 0) {
-            $delayedCallback->insert("messages", "s", [ "message" => $message ]);
-          }
-        } elseif ($comment) {
-          $delayedCallback->insert("messages", "ss", [ "message" => $message, "comment" => $comment ]);
-        } else {
-          $delayedCallback->insert("messages", "s", [ "message" => $message ]);
-        }
-      });
+      DelayedMethodCalls::stack($this, "insertMessage", [ $message, $options ]);
+      return $message;
     }
-    if ($translation) {
-      return $translation;
-    }
-    return $message;
   }
 
   public function getRoute($route, $languageCode, $options) {
@@ -99,6 +78,39 @@ class I18nModel extends AbstractModel {
     //
     //return $this->method($route, $languageCode, $options);
     return $route;
+  }
+
+  /**
+   * Insert a message for translation into the messages database table.
+   *
+   * @param string $message
+   *   The message to insert.
+   * @param array $options
+   *   Associative array to overwrite the default options used in this method. Possible keys are:
+   *   <ul>
+   *     <li><tt>comment</tt>: default is <tt>NULL</tt>.</li>
+   *     <li><tt>old_message</tt>: default is <tt>NULL</tt>.</li>
+   *   </ul>
+   * @return \MovLib\Model\I18nModel
+   */
+  public function insertMessage($message, $options) {
+    $issetComment = isset($options["comment"]);
+    self::$mysqli->affected_rows = 0;
+    if (isset($options["old_message"])) {
+      if ($issetComment) {
+        $this->query("UPDATE `messages` SET `message` = ?, `comment` = ? WHERE `message` = ? LIMIT 1", "s", $options["old_message"]);
+      } else {
+        $this->query("UPDATE `messages` SET `message` = ? WHERE `message` = ? LIMIT 1", "s", $options["old_message"]);
+      }
+    }
+    if (self::$mysqli->affected_rows === 0) {
+      if ($issetComment) {
+        $this->query("INSERT INTO `messages` (`message`, `comment`) VALUES (?, ?)", "ss", [ $message, $options["comment"] ]);
+      } else {
+        $this->query("INSERT INTO `message` (`message`) VALUES (?)", "s", $message);
+      }
+    }
+    return $this;
   }
 
 }
