b140a1f09202aeb386fe1bbb9a8a7a77e8dca54f
Richard Fussenegger <richard@fussenegger.info>
2013-06-12 13:56:50 +0200

    Removed unused methods.

--- src/MovLib/Utility/String.php
+++ src/MovLib/Utility/String.php
@@ -57,59 +57,6 @@ class String {
   }
 
   /**
-   * Formats a string to HTML display by replacing variable placeholders.
-   *
-   * This function replaces variable placeholders in a string with the requested values and escapes the values so they
-   * can be safely displayed as HTML. It should be used on any unknown text that is intended to be printed to an HTML
-   * page (especially text that may have come from untrusted users, since in that case it prevents cross-site scripting
-   * and other security problems).
-   *
-   * <b>Usage example:</b>
-   * <pre>String::format("Hello @world! Hello %world! Hello !world!", [ "@world" => "W<>orld", "%world" => "W<>orld", "!world" => "W<>orld" ]);</pre>
-   * Output:
-   * <pre>Hello W&amp;lt;&amp;gt;ld! Hello <em class='placeholder'>W&amp;lt;&amp;gt;rld</em>! Hello W<>orld!</pre>
-   *
-   * @link http://api.drupal.org/api/drupal/core!lib!Drupal!Component!Utility!String.php/function/String%3A%3Aformat/8
-   * @see \MovLib\Utility\String::checkPlain()
-   * @see \MovLib\Utility\String::placeholder()
-   * @param string $string
-   *   String containing placeholders.
-   * @param array $args
-   *   An associative array of replacements to make. Occurrences in $string of any key in $args are replaced with the
-   * corresponding value, after optional sanitization and formatting. The type of sanitization and formatting depends on
-   * the first character of the key:
-   *   <ul>
-   *     <li><b>@variable:</b> Excaped to HTML using <code>String::checkPlain()</code>. Use this as the default choice
-   *     for anything displayed on a page on the site.</li>
-   *     <li><b>%variable:</b> Escaped to HTML and formatted using <code>String::placeholder()</code>, which makes it
-   *     display as <em>emphasized</em> text.</li>
-   *     <li><b>!variable:</b> Inserted as is, with no sanitization or formatting. Only use this for text that has
-   *     already been prepared for HTML display (for example, user-supplied text that has already been run through
-   *     <code>String::checkPlain()</code> previously, or is expected to contain some limited HTML tags and has already
-   *     been run through <code>String::filterXss()</code> previously).</li>
-   *   </ul>
-   * @return string
-   *   The formatted string with placeholders inserted, or <tt>FALSE</tt> if no args specified.
-   */
-  public static function format($string, $args = []) {
-    foreach ($args as $key => $value) {
-      switch ($key[0]) {
-        case "@":
-          $args[$key] = self::checkPlain($value);
-          break;
-
-        case "%":
-          $args[$key] = self::placeholder($value);
-          break;
-
-        case "!":
-          // Pass-through!
-      }
-    }
-    return strtr($string, $args);
-  }
-
-  /**
    * Formats text for emphasized display in a placeholder inside a sentence.
    *
    * Used automatically by <code>String::format()</code>.
