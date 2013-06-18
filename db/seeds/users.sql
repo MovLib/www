72ae8a6473222d9cb533e5b98afa6bcb79db7f09
Richard Fussenegger <richard@fussenegger.info>
2013-06-18 14:16:19 +0200

    Always a newline at the end of a file. Updated seed data for users.

--- db/seeds/users.sql
+++ db/seeds/users.sql
@@ -1,24 +1,24 @@
 USE `movlib`;
 BEGIN;
 INSERT INTO `users` (
-    `name`,
-    `mail`,
-    `pass`,
-    `created`,
-    `login`,
-    `timezone`,
-    `init`,
-    `dyn_data`,
-    `language_id`
-  ) VALUES (
-    'test',
-    'test@example.com',
-    '$2y$10$zxXKIGS8N9z6vk6iAPiR1u5h2Eypz7kWOQRRfa1uXZ1igkjQ1F8Ga', -- Hashed password "test"
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP,
-    'UTC',
-    'test@old.com',
-    '',
-    41
+  `language_id`,
+  `name`,
+  `mail`,
+  `pass`,
+  `created`,
+  `login`,
+  `timezone`,
+  `init`,
+  `dyn_data`
+) VALUES (
+  41,
+  'testname',
+  'test@example.com',
+  '$2y$10$zxXKIGS8N9z6vk6iAPiR1u5h2Eypz7kWOQRRfa1uXZ1igkjQ1F8Ga', -- Hashed password "test"
+  CURRENT_TIMESTAMP,
+  CURRENT_TIMESTAMP,
+  'UTC',
+  'test@old.com',
+  ''
 );
 COMMIT;
