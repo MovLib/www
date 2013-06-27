6fe223369e863f47a174dc1abceb337b367eaf6f
Richard Fussenegger <richard@fussenegger.info>
2013-06-27 09:19:52 +0200

    Real seed data for the user.

--- db/seeds/users.sql
+++ db/seeds/users.sql
@@ -10,17 +10,27 @@ INSERT INTO `users` (
   `timezone`,
   `init`,
   `dyn_profile`,
-  `avatar_ext`
+  `country_id`,
+  `avatar_ext`,
+  `real_name`,
+  `birthday`,
+  `gender`,
+  `website`
 ) VALUES (
-  41,
-  'testname',
-  'test@example.com',
+  (SELECT `language_id` FROM `languages` WHERE `iso_alpha-2` = 'en' LIMIT 1),
+  'Fleshgrinder',
+  'richard@fussenegger.info',
   '$2y$10$zxXKIGS8N9z6vk6iAPiR1u5h2Eypz7kWOQRRfa1uXZ1igkjQ1F8Ga', -- Hashed password "test"
   CURRENT_TIMESTAMP,
   CURRENT_TIMESTAMP,
-  'UTC',
-  'test@old.com',
-  '',
-  'svg'
+  'Europe/Vienna',
+  'richard@fussenegger.info',
+  COLUMN_CREATE('en', 'Richard’s English profile text.', 'de', 'Richard’s deutscher Profiltext.'),
+  (SELECT `country_id` FROM `countries` WHERE `iso_alpha-2` = 'at' LIMIT 1),
+  'jpg',
+  'Richard Fussenegger',
+  '1985-6-27',
+  1,
+  'http://richard.fussenegger.info/'
 );
 COMMIT;
