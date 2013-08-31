0848c31a1086d3d1fc7080cb2259f5e20255a525
Richard Fussenegger <richard@fussenegger.info>
2013-09-01 01:54:59 +0200

    Optimized @markus please review

--- src/MovLib/View/HTML/MoviesView.php
+++ src/MovLib/View/HTML/MoviesView.php
@@ -36,22 +36,22 @@ class MoviesView extends AbstractPageView {
    * Initialize new MoviesView.
    *
    * @param \MovLib\Presenter\MoviesPresenter $presenter
-   *  The presenter controlling this view.
+   *   The presenter controlling this view.
    * @param string $title
-   *  The page's title.
+   *   The page's title.
    */
   public function __construct($presenter, $title) {
-    parent::__construct($presenter, $title);
+    $this->init($presenter, $title);
     $this->stylesheets[] = "modules/movies.css";
   }
 
   /**
-   *
-   * @global \MovLib\Model\I18nModel $i18n
-   *   The global I18n Model instance for translations.
+   * @inheritdoc
    */
   public function getContent() {
     global $i18n;
+
+    // @todo Move to presenter!
     $secondaryNavPoints = [
       [ $i18n->r("/movies"), $i18n->t("Movies"), [
         "title" => $i18n->t("View the latest {0} additions to the database.", [ $i18n->t("movie") ])
@@ -64,67 +64,71 @@ class MoviesView extends AbstractPageView {
       ]]
     ];
 
+    // @todo Move to presenter!
     $moviesList = "<ol id='movies__latest'>";
     $movies = $this->presenter->moviesModel->getMoviesByCreated();
     $c = count($movies);
     for ($i = 0; $i < $c; ++$i) {
+      // @todo Remove exception!
       try {
         $poster = $movies[$i]["#movie"]->getPosterDisplay();
       } catch (MovieException $e) {
         $poster = new MoviePosterModel();
       }
-      if (!empty($movies[$i]["#movie"]->getTitleDisplay())) {
-        $title = $movies[$i]["#movie"]->getTitleDisplay()["title"];
-      }
-      else {
-        $title = $movies[$i]["#movie"]->originalTitle;
-      }
-      $countriesAndYear = [];
-      if (empty($movies[$i]["#movie"]->getCountries()) === false) {
-        $cc = count($movies[$i]["#movie"]->getCountries());
-        for ($ci = 0; $ci < $cc; ++$ci) {
-          $countriesAndYear[] = $movies[$i]["#movie"]->getCountries()[$ci]["code"];
+
+      // Try to get the display title and fallback to the original title.
+      $movieDisplayTitle = $movies[$i]["#movie"]->getTitleDisplay();
+      $title = !empty($movieDisplayTitle) ? $movieDisplayTitle : $movies[$i]["#movie"]->originalTitle;
+
+      // Collect countries and the year.
+      $countriesAndYear = "";
+      $countries = $movies[$i]["#movie"]->getCountries();
+      $cc = count($countries);
+      for ($ci = 0; $ci < $cc; ++$ci) {
+        if ($ci !== 0) {
+          $countriesAndYear .= ", ";
         }
+        $countriesAndYear .= $countries[$ci]["code"];
       }
       if (isset($movies[$i]["#movie"]->year)) {
-        $countriesAndYear[] = "{$movies[$i]["#movie"]->year}";
+        if (!empty($countriesAndYear)) {
+          $countriesAndYear .= ", ";
+        }
+        $countriesAndYear .= "{$movies[$i]["#movie"]->year}";
       }
-      $countriesAndYear = $this->getCommaSeparatedList($countriesAndYear, "");
-      if (strlen($countriesAndYear) > 0) {
+      if (!empty($countriesAndYear)) {
         $countriesAndYear = " ({$countriesAndYear})";
       }
+
       $moviesList .=
-        "<li>" .
-          $this->a(
-            $i18n->r("/movie/{0}", [ $movies[$i]["#movie"]->id ]),
-            "<article>" .
-              "<div class='movies-list__poster'>" .
-                $this->getImage($poster,
-                  MoviePosterModel::IMAGESTYLE_SMALL,
-                  [ "alt" => $i18n->t("{0} movie poster.", [ $title ]) ]) .
-              "</div>" .
-              "<div class='movies-list__info clear-fix'>" .
-                "<h2>{$title}{$countriesAndYear}</h2>" .
-                "<p>{$i18n->t("“{0}” (<em>original title</em>)", [ $movies[$i]["#movie"]->originalTitle ])}</p>" .
-              "</div>" .
-            "</article>",
-            [ "tabindex" => $this->getTabindex() ]
-          ) .
-        "</li>";
+        "<li>{$this->a(
+          $i18n->r("/movie/{0}", [ $movies[$i]["#movie"]->id ]),
+          "<article>" .
+            "<div class='movies-list__poster'>{$this->getImage($poster, MoviePosterModel::IMAGESTYLE_SMALL, [
+              "alt" => $i18n->t("{0} movie poster.", [ $title ])
+            ])}</div>" .
+            "<div class='movies-list__info clear-fix'>" .
+              "<h2>{$title}{$countriesAndYear}</h2>" .
+              "<p>{$i18n->t("“{0}” (<em>original title</em>)", [ $movies[$i]["#movie"]->originalTitle ])}</p>" .
+            "</div>" .
+          "</article>",
+          [ "tabindex" => $this->getTabindex() ]
+        )}</li>"
+      ;
     }
     $moviesList .= "</ol>";
+
     return
       "<div class='container'>" .
-        "<div class='row'>" .
-          $i18n->t("On the {0}movies{1} page you can find the latest movies that have been added to our database.", [ "<strong>", "</strong>" ]) .
-        "</div>" .
+        "<div class='row'>{$i18n->t("On the {0}movies{1} page you can find the latest movies that have been added to our database.", [
+          "<strong>", "</strong>"
+        ])}</div>" .
         "<div class='row'>" .
           "<aside class='span span--3'>{$this->getSecondaryNavigation($i18n->t("Sort the movie entries"), $secondaryNavPoints)}</aside>" .
-          "<div class='span span--9'>" .
-            $moviesList .
-          "</div>" .
+          "<div class='span span--9'>{$moviesList}</div>" .
         "</div>" .
-      "</div>";
+      "</div>"
+    ;
   }
 
 }
