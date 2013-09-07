8a0030d24940772aef922fb0c122ddf8ddc7817a
Richard Fussenegger <richard@fussenegger.info>
2013-09-07 11:27:24 +0200

    Overhauled mailer system to use sendmail (exim4)

--- src/MovLib/Utility/DelayedMailer.php
+++ src/MovLib/Utility/DelayedMailer.php
@@ -18,10 +18,9 @@
 namespace MovLib\Utility;
 
 use \DateTime;
-use \MovLib\Exception\ErrorException;
 use \MovLib\Exception\MailerException;
-use \MovLib\Utility\DelayedLogger;
 use \MovLib\Utility\String;
+use \MovLib\View\Mail\AbstractMail;
 
 /**
  * Delayed mailer system.
@@ -36,66 +35,8 @@ use \MovLib\Utility\String;
 class DelayedMailer {
 
 
-  // ------------------------------------------------------------------------------------------------------------------- Constants
-
-
-  /**
-   * If set to <code>TRUE</code> all SMTP responses will be written in real-time to the debug log.
-   *
-   * @var boolean
-   */
-  const DEBUG = false;
-
-  /**
-   * SMTP connect timeout in seconds.
-   *
-   * @var int
-   */
-  const CONNECT_TIMEOUT = 30;
-
-  const HEADER_ENCODE_PHRASE = 0;
-
-  const HEADER_ENCODE_COMMENT = 1;
-
-  const HEADER_ENCODE_TEXT = 2;
-
-  /**
-   * Each header line must have length <= 75, including start (=?utf-8?B?) and end (?=).
-   *
-   * @var int
-   */
-  const HEADER_MAX_LENGTH = 63;
-
-  /**
-   * SMTP response timeout in seconds.
-   *
-   * @var int
-   */
-  const RESPONSE_TIMEOUT = 15;
-
-  /**
-   * SMTP read timeout in seconds.
-   *
-   * @var int
-   */
-  const READ_TIMEOUT = 30;
-
-
   // ------------------------------------------------------------------------------------------------------------------- Properties
 
-
-  /**
-   * Socket connection to the server.
-   *
-   * @var resource
-   */
-  private $connection;
-
-  /**
-   *
-   * @var boolean
-   */
-  public $keepAlive = true;
 
   /**
    * Array used to collect all mails that should be sent.
@@ -104,118 +45,9 @@ class DelayedMailer {
    */
   private static $mails = [];
 
-  /**
-   * The last response message from the server.
-   *
-   * @var string
-   */
-  private $responseMessage = "";
-
-  /**
-   * The last response code from the server.
-   *
-   * @var int
-   */
-  private $responseCode = 0;
-
-
-  // ------------------------------------------------------------------------------------------------------------------- Magic Methods
-
-
-  /**
-   * Disconnect gracefully from SMTP server if a connection is open upon destruction of this instance.
-   */
-  public function __destruct() {
-    if ($this->connected()) {
-      $this->disconnect();
-    }
-  }
-
 
   // ------------------------------------------------------------------------------------------------------------------- Public Methods
 
-
-  /**
-   * Establish TLS encrypted and authenticated connection to SMTP server.
-   *
-   * @return this
-   * @throws \MovLib\Exception\MailerException
-   */
-  public function connect() {
-    if (!$this->connected()) {
-      try {
-        $this
-          ->debug("Establishing stream socket connection to SMTP host {$GLOBALS["conf"]["mail"]["host"]} on port {$GLOBALS["conf"]["mail"]["port"]}.")
-          ->connection = stream_socket_client("{$GLOBALS["conf"]["mail"]["host"]}:{$GLOBALS["conf"]["mail"]["port"]}", $errno, $errstr, self::CONNECT_TIMEOUT, STREAM_CLIENT_CONNECT, stream_context_create())
-        ;
-      } catch (ErrorException $e) {
-        throw new MailerException("Could not connect to SMTP server. SOCKET ERROR {$errno}: {$errstr}", $e);
-      }
-      // Check if server sent proper announce.
-      if ($this->serverResponse()->responseCode !== 220) {
-        throw new MailerException("Did not receive SMTP announcement. SMTP ERROR {$this->responseCode}: {$this->responseMessage}");
-      }
-
-      // Snychronize with SMTP server and encrypt connection.
-      $this->hello()->clientSend("STARTTLS\r\n");
-      if ($this->responseCode !== 220) {
-        throw new MailerException("Could not innitiate STARTTLS communication with SMTP server. SMTP ERROR {$this->responseCode}: {$this->responseMessage}");
-      }
-      if (!stream_socket_enable_crypto($this->connection, true, STREAM_CRYPTO_METHOD_TLS_CLIENT)) {
-        throw new MailerException("Could not encrypt SMTP stream.");
-      }
-
-      // We have to re-synch after establishing the encrypted communication and afterwards we can log in with our user.
-      $this->hello()->clientSend("AUTH CRAM-MD5\r\n");
-      if ($this->responseCode !== 334) {
-        throw new MailerException("Authentication method not accepted from SMTP server. SMTP ERROR {$this->responseCode}: {$this->responseMessage}");
-      }
-      $challenge = base64_decode(substr($this->responseMessage, 4));
-      $password = hash_hmac("md5", $challenge, $GLOBALS["conf"]["mail"]["password"]);
-      $this->clientSend(base64_encode("{$GLOBALS["conf"]["mail"]["username"]} {$password}") . "\r\n");
-      if ($this->responseCode !== 235) {
-        throw new MailerException("Credentials not accepted from SMTP server. SMTP ERROR {$this->responseCode}: {$this->responseMessage}");
-      }
-    }
-    return $this;
-  }
-
-  /**
-   * Check if we are connected to the SMTP server.
-   *
-   * @return boolean
-   *   <code>TRUE</code> if connected, otherwise <code>FALSE</code>.
-   */
-  public function connected() {
-    if (is_resource($this->connection)) {
-      if (!stream_get_meta_data($this->connection)["eof"]) {
-        return true;
-      }
-      $this->debug("EOF caught while checking if connected to SMTP server.");
-      $this->closeConnection();
-    }
-    return false;
-  }
-
-  /**
-   * Send quit command to SMTP server and then closes the socket if there is no error.
-   *
-   * Implements <code>QUIT <CRLF></code> from RFC 821.
-   *
-   * @link http://www.ietf.org/rfc/rfc2821.txt
-   * @return this
-   * @throws \MovLib\Exception\MailerException
-   */
-  public function disconnect() {
-    if (is_resource($this->connection)) {
-      $this->clientSend("quit\r\n");
-      if ($this->responseCode !== 221) {
-        throw new MailerException("SMTP server rejected quit command. SMTP ERROR {$this->responseCode}: {$this->responseMessage}");
-      }
-      $this->closeConnection();
-    }
-    return $this;
-  }
 
   /**
    * Send the given mail.
@@ -228,66 +60,36 @@ class DelayedMailer {
    * @return this
    * @throws MailerException
    */
-  public function send($mail) {
+  public function send(AbstractMail $mail) {
     global $i18n;
-
-    if (method_exists($mail, "init")) {
-      $mail->init();
-    }
-
-    $this->clientSend("MAIL FROM:<{$GLOBALS["conf"]["email"]}>\r\n");
-    if ($this->responseCode !== 250) {
-      throw new MailerException("SMTP server refused mail from ({$GLOBALS["conf"]["email"]}). SMTP ERROR {$this->responseCode}: {$this->responseMessage}");
-    }
-
-    $this->clientSend("RCPT TO:<{$mail->recipient}>\r\n");
-    if ($this->responseCode !== 250 && $this->responseCode !== 251) {
-      throw new MailerException("SMTP server refused recipient to ({$mail->recipient}). SMTP ERROR {$this->responseCode}: {$this->responseMessage}");
-    }
-
-    $this->clientSend("DATA\r\n");
-    if ($this->responseCode !== 354) {
-      throw new MailerException("SMTP server refused data command. SMTP ERROR {$this->responseCode}: {$this->responseMessage}");
-    }
-
-    // Build the message ID following common best practices.
-    $messageId = String::base36encode(microtime(true)) . "." . String::base36encode(md5("{$GLOBALS["conf"]["mail"]["from"]}.{$mail->recipient}"));
-
-    // @todo Split DATA every 1000 characters!
-    $this->clientSend(implode("\r\n", [
-      "Content-Transfer-Encoding: 8bit",
-      "Content-Type: multipart/alternative;",
-      "\tboundary=\"{$messageId}\"",
-      "Date: " . date(DateTime::RFC822),
-      "From: {$this->encodeHeader('"' . $i18n->t($GLOBALS["conf"]["mail"]["from_name"]) . '"')} <{$GLOBALS["conf"]["mail"]["from"]}>",
-      "Message-ID: <{$messageId}@{$_SERVER["SERVER_NAME"]}>",
-      "MIME-Version: 1.0",
-      "Return-Path: <{$GLOBALS["conf"]["mail"]["from"]}>",
-      "Subject: {$this->encodeHeader(String::collapseWhitespace($mail->subject))}",
-      "To: {$mail->recipient}",
-      "",
-      "--{$messageId}",
-      "Content-Type: text/plain; charset=utf-8",
-      "Content-Transfer-Encoding: 8bit",
-      "",
-      String::wordwrap(str_replace(array("\r", "\r\n"), "\n", $mail->getPlain()), 80, "\n", true),
-      "",
-      "--{$messageId}",
-      "Content-Type: text/html; charset=utf-8",
-      "Content-Transfer-Encoding: 8bit",
-      "",
-      String::collapseWhitespace($mail->getHtml()),
-      "",
-      "--{$messageId}--",
-      "",
-      ".",
-      ""
-    ]));
-    if ($this->responseCode !== 250) {
-      throw new MailerException("SMTP server refused data payload. SMTP ERROR {$this->responseCode}: {$this->responseMessage}");
-    }
-
-    $this->keepAlive === true ? $this->reset() : $this->disconnect();
+    $messageId = uniqid("movlib");
+    mail(
+      $mail->recipient,
+      mb_encode_mimeheader($mail->subject),
+      implode("\n", [
+        "--{$messageId}",
+        "Content-Type: text/plain; charset=utf-8",
+        "Content-Transfer-Encoding: BASE64",
+        "",
+        base64_encode(String::wordwrap($mail->getPlain())),
+        "",
+        "--{$messageId}",
+        "Content-Type: text/html; charset=utf-8",
+        "Content-Transfer-Encoding: BASE64",
+        "",
+        base64_encode(String::collapseWhitespace($mail->getHtml())),
+        "",
+        "--{$messageId}--",
+      ]),
+      implode("\n", [
+        "Content-Type: multipart/alternative;",
+        "\tboundary=\"{$messageId}\"",
+        "From: \"" . mb_encode_mimeheader($i18n->t($GLOBALS["movlib"]["default_from_name"])) . "\" <{$GLOBALS["movlib"]["default_from"]}>",
+        "Message-ID: <{$messageId}@{$GLOBALS["movlib"]["default_domain"]}>",
+        "MIME-Version: 1.0",
+      ]),
+      "-f {$GLOBALS["movlib"]["default_from"]}"
+    );
     return $this;
   }
 
@@ -299,25 +101,12 @@ class DelayedMailer {
    * Sends all mails in the stack.
    */
   public static function run() {
-    $mailer = (new DelayedMailer())->connect();
+    $mailer = new DelayedMailer();
     $c = count(self::$mails);
     for ($i = 0; $i < $c; ++$i) {
       $mailer->send(self::$mails[$i]);
     }
-    $mailer->disconnect();
   }
-
-  /**
-   * Add mail to the stack.
-   *
-   * @param string|array $recipient
-   *   Recipient(s) of the mail, must comply with RFC 2822. If more then one recipient should receive this mail, all
-   *   mails must be passed as numeric array.
-   * @param string $subject
-   *   The mail's subject, must comply with RFC 2047.
-   * @param string $htmlBody
-   *
-   */
 
   /**
    * Add mail to stack.
@@ -328,266 +117,6 @@ class DelayedMailer {
   public static function stack($mail) {
     delayed_register(__CLASS__, 0);
     self::$mails[] = $mail;
-  }
-
-
-  // ------------------------------------------------------------------------------------------------------------------- Private Methods
-
-
-  /**
-   * Send data to the server.
-   *
-   * @see \MovLib\View\Mail\AbstractMailer::serverResponse()
-   * @param string $data
-   *   The data to send.
-   * @return this
-   * @throws \MovLib\Exception\MailerException
-   */
-  private function clientSend($data) {
-    if (fwrite($this->connection, $data) === false) {
-      throw new MailerException("Sending data to SMTP server failed. Data was:\n\n{$data}");
-    }
-    return $this->debug("Sent: {$data}")->serverResponse();
-  }
-
-  /**
-   * Close the socket and clean up. You should always call <code>Mailer::quit()</code> before calling this method.
-   *
-   * @return this
-   */
-  private function closeConnection() {
-    if (is_resource($this->connection)) {
-      fclose($this->connection);
-      $this->connection = null;
-    }
-    return $this;
-  }
-
-  /**
-   * Writes the given message to the debug log if the class constants <var>DelayedMailer::DEBUG</var> is set to
-   * <code>TRUE</code>.
-   *
-   * <b>HINT:</b> Use the following command in your shell to check the debug log in real-time:
-   * <code>tail -f /var/www/logs/debug-smtp.log</code>
-   *
-   * @param string $message
-   *   The message to log.
-   * @return this
-   */
-  private function debug($message) {
-    if (self::DEBUG === true) {
-      // Passing a string as level will create a separate log file for us.
-      DelayedLogger::logNow(rtrim($message, PHP_EOL) . PHP_EOL, "debug-smtp");
-    }
-    return $this;
-  }
-
-  /**
-   * Enoce a header string to the best (shortest) of B, Q, quoted or none encoding.
-   *
-   * @param string $header
-   *   The header to encode.
-   * @param int $position
-   *   The position of the header. Use the class constants <var>DelayedMailer::HEADER_ENCODE_*</var>.
-   * @return string
-   *   The encoded header.
-   */
-  private function encodeHeader($header, $position = self::HEADER_ENCODE_TEXT) {
-    $x = null;
-    $matches = null;
-
-    switch ($position) {
-      case self::HEADER_ENCODE_PHRASE:
-        if (!preg_match('/[\200-\377]/', $header)) {
-          $encoded = addcslashes($header, "\0..\37\177\\\"");
-          if (($header === $encoded) && !preg_match('/[^A-Za-z0-9!#$%&\'*+\/=?^_`{|}~ -]/', $header)) {
-            return $encoded;
-          }
-          else {
-            return '"' . $encoded . '"';
-          }
-        }
-        $x = preg_match_all('/[^\040\041\043-\133\135-\176]/', $header, $matches);
-        break;
-
-      case self::HEADER_ENCODE_COMMENT:
-        $x = preg_match_all('/[()"]/', $header, $matches);
-        // Fall through
-
-      case self::HEADER_ENCODE_TEXT:
-      default:
-        $x += preg_match_all('/[\000-\010\013\014\016-\037\177-\377]/', $header, $matches);
-    }
-
-    // No matches and therefor nothing to encode.
-    if (!$x) {
-      return $header;
-    }
-
-    // Try to select the encoding which should produce the shortest output.
-    // If more than a third of the content will need encoding, B encoding is most efficient.
-    if ($x > mb_strlen($header) / 3) {
-      $encoding = "B";
-      $encoded = $this->encodeHeaderBencode($header);
-    }
-    else {
-      $encoding = "Q";
-      $encoded = $this->encodeHeaderQencode($header, $position);
-    }
-    return preg_replace("/^(.*)$/m", " =?utf-8?{$encoding}?\\1?=", $encoded);
-  }
-
-  /**
-   * Bencode the given header.
-   *
-   * @link https://en.wikipedia.org/wiki/Bencode
-   * @param string $header
-   *   The header to encode.
-   * @return string
-   *   The encoded header.
-   */
-  private function encodeHeaderBencode($header) {
-    $encoded = "";
-    $mbLength = mb_strlen($header);
-    if ($mbLength === strlen($header)) {
-      $encoded = trim(chunk_split($header, self::HEADER_MAX_LENGTH % 4, "\n"));
-    }
-    else {
-      // Base64 has a 4:3 ratio.
-      $offset = $avgLength = floor(self::HEADER_MAX_LENGTH * ($mbLength / strlen($header)) * 0.75);
-      for ($i = 0; $i < $mbLength; $i += $offset) {
-        $lookBack = 0;
-        do {
-          $offset = $avgLength - $lookBack;
-          $chunk = base64_encode(mb_substr($header, $i, $offset));
-          ++$lookBack;
-        }
-        while (strlen($chunk) > self::HEADER_MAX_LENGTH);
-        if ($i !== 0) {
-          $encoded .= "\n";
-        }
-        $encoded .= $chunk;
-      }
-    }
-    return $encoded;
-  }
-
-  /**
-   * Qencode the given header.
-   *
-   * @param string $header
-   *   The header to encode.
-   * @param int $position
-   *   The position of the header. Use the class constants <var>DelayedMailer::HEADER_ENCODE_*</var>.
-   * @return string
-   *   The encoded header.
-   */
-  private function encodeHeaderQencode($header, $position) {
-    $pattern = "";
-    $encoded = String::collapseWhitespace($header);
-    switch ($position) {
-      case self::HEADER_ENCODE_PHRASE:
-        $pattern = '^A-Za-z0-9!*+\/ -';
-        break;
-
-      case self::HEADER_ENCODE_COMMENT:
-        $pattern = '\(\)"';
-        // Fall through
-
-      case self::HEADER_ENCODE_TEXT:
-        $pattern = '"\075\000-\011\013\014\016-\037\077\137\177-\377' . $pattern;
-    }
-    if (preg_match_all('/[' . $pattern . ']/', $encoded, $matches)) {
-      foreach (array_unique($matches[0]) as $char) {
-        $encoded = str_replace($char, "=" . sprintf("%02X", ord($char)), $encoded);
-      }
-    }
-    return str_replace(" ", "_", $encoded);
-  }
-
-  /**
-   * Sends <code>EHLO</code> (extended hello) or <code>HELO</code> command to the SMTP server. This makes sure that we
-   * and the server are in the same known state.
-   *
-   * Implements RFC 821: <code>HELO <SP> <domain> <CRLF></code>
-   *
-   * @link http://www.ietf.org/rfc/rfc2821.txt
-   * @see \MovLib\View\Mail\AbstractMailer::helloSend()
-   * @return this
-   * @throws \MovLib\Exception\MailerException
-   */
-  private function hello() {
-    if (!$this->helloSend("EHLO")) {
-      $e = new MailerException("SMTP server did not accept EHLO command. SMTP ERROR {$this->responseCode}: {$this->responseMessage}");
-      if (!$this->helloSend("HELO")) {
-        throw new MailerException("SMTP server did not accept EHLO nor HELO command. SMTP ERROR {$this->responseCode}: {$this->responseMessage}", $e);
-      }
-    }
-    return $this;
-  }
-
-  /**
-   * Send <code>EHLO</code> or <code>HELO</code> command.
-   *
-   * @param string $command
-   *   Either <code>EHLO</code> or <code>HELO</code>.
-   * @return boolean
-   *   <code>TRUE</code> if command succeeded, otherwise <code>FALSE</code>.
-   */
-  private function helloSend($command) {
-    $this->clientSend("{$command} {$GLOBALS["conf"]["mail"]["host"]}\r\n");
-    return $this->responseCode === 250;
-  }
-
-  /**
-   * Sendes the <code>RSET</code> command to abort a transaction that is currently in progress.
-   *
-   * @return this
-   * @throws \MovLib\Exception\MailerException
-   */
-  private function reset() {
-    $this->clientSend("RSET\r\n");
-    if ($this->responseCode !== 250) {
-      throw new MailerException("SMTP server refused RSET command. SMTP ERROR {$this->responseCode}: {$this->responseMessage}");
-    }
-    return $this;
-  }
-
-  /**
-   * Read as many lines as possible from the SMTP response, either before EOF or socket timeout.
-   *
-   * If the 4th character in the response is a dash (-) we have more lines to read. There are no more lines to read if
-   * it is a space.
-   *
-   * Use the properties <code>Mailer::$responseMessage</code> and <code>Mailer::$responseCode</code> to access the
-   * response after calling this method.
-   *
-   * @return this
-   * @throws \MovLib\Exception\MailerException
-   */
-  private function serverResponse() {
-    $response = "";
-    $endTime = time() + self::READ_TIMEOUT;
-    stream_set_timeout($this->connection, self::RESPONSE_TIMEOUT);
-    while (is_resource($this->connection) && !feof($this->connection)) {
-      $response .= $tmp = fgets($this->connection, 515);
-      // If the 4th character is a space, we are done reading.
-      if (substr($tmp, 3, 1) === " ") {
-        break;
-      }
-      $responseTimedout = stream_get_meta_data($this->connection)["timed_out"];
-      $readTimedout = time() > $endTime;
-      if ($responseTimedout || $readTimedout) {
-        $this->disconnect();
-        throw new MailerException($responseTimedout
-          ? "SMTP timed out (response took more then " . self::RESPONSE_TIMEOUT . " seconds)."
-          : "SMTP timelimit reached (read took more then " . self::READ_TIMEOUT . " seconds)."
-        );
-      }
-    }
-    $this->responseMessage = $response;
-    $this->responseCode = (int) substr($response, 0, 3);
-    return $this->debug($response);
   }
 
 }
