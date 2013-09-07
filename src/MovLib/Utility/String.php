407a47672a74ebff828b30268c8f1fbd3f14e501
Richard Fussenegger <richard@fussenegger.info>
2013-09-07 11:25:33 +0200

    Implemented working multi-byte wordwrap method and updated tests

--- src/MovLib/Utility/String.php
+++ src/MovLib/Utility/String.php
@@ -29,32 +29,6 @@ namespace MovLib\Utility;
 class String {
 
   /**
-   * Encodes data with base 36.
-   *
-   * @link https://en.wikipedia.org/wiki/Base_36
-   * @param mixed $data
-   *   The data to encode.
-   * @return string
-   *   The encoded data, as a string or <code>FALSE</code> on failure.
-   */
-  public static function base36encode($data) {
-    return base_convert($data, 10, 36);
-  }
-
-  /**
-   * Decode data with base 36.
-   *
-   * @link https://en.wikipedia.org/wiki/Base_36
-   * @param string $data
-   *   The encoded data.
-   * @return mixed
-   *   Returns the original data or <code>FALSE</code> on failure.
-   */
-  public static function base36decode($data) {
-    return base_convert($data, 36, 10);
-  }
-
-  /**
    * Encodes special characters in a plain-text string for display as HTML.
    *
    * Also validates strings as UTF-8.
@@ -131,30 +105,6 @@ class String {
   }
 
   /**
-   * Shorten given string to given length and optionally append a string.
-   *
-   * <b>Usage example:</b>
-   * The following will return “<em>extre…</em>”:
-   * <pre>String::shorten("extremely long string", 5, __("…"));</pre>
-   *
-   * @param string $string
-   *   The string to shorten.
-   * @param int $length
-   *   The length the string shall not exceed.
-   * @param string $append
-   *   [Optional] String to append if the string exceeds <var>$length</var>. Defaults to an empty string.
-   * @return string
-   *   The string as is if it does not exceed <var>$length</var>, otherwise the shortened string with <var>$append</var>
-   *   appended to the end of the string.
-   */
-  public static function shorten($string, $length, $append = "") {
-    if (mb_strlen($string) > $length) {
-      return mb_substr($string, 0, $length) . $append;
-    }
-    return $string;
-  }
-
-  /**
    * Collapse all kinds of whitespace characters to a single whitespace.
    *
    * @param string $string
@@ -180,51 +130,62 @@ class String {
   }
 
   /**
-   * Multi-byte aware implementation of PHP's wordwrap function.
+   * Multi-byte aware wordwrap implementation.
+   *
+   * Please note that this function will always normalize line feeds to LF.
    *
    * @see wordwrap()
+   * @link https://api.drupal.org/api/drupal/core%21vendor%21zendframework%21zend-stdlib%21Zend%21Stdlib%21StringWrapper%21AbstractStringWrapper.php/function/AbstractStringWrapper%3A%3AwordWrap/8
    * @param string $string
    *   The string to wrap.
-   * @param int $width
-   *   The number of characters at which the string will be wrapped.
-   * @param string $break
-   *   [Optional] The line is broken with this string. Defaults to <var>PHP_EOL</var>.
-   * @param boolean $cut
-   *   [Optional] If set to <code>TRUE</code>, the string is always wrapped at or before the specified width. So if
-   *   you have a word that is larger than the given width, it is broken apart. Defaults to <code>FALSE</code>.
+   * @param int $width [optional]
+   *   The number of characters at which the string will be wrapped, defaults to <code>75</code>.
+   * @param boolean $cut [optional]
+   *   If set to <code>TRUE</code>, the string is always wrapped at or before the specified width. So if you have a word
+   *   that is larger than the given width, it is broken apart. Defaults to <code>FALSE</code>.
    * @return string
    *   The string wrapped at the specified length.
    */
-  public static function wordwrap($string, $width, $break = PHP_EOL, $cut = false) {
+  public static function wordwrap($string, $width = 75, $cut = false) {
     // Always remove whitespaces at beginning and end, nobody needs them.
-    $string = trim($string);
-    // Check if we are dealing with a multi-byte string, if not use native function.
+    $string = trim(self::normalizeLineFeeds($string));
+    // Use native function if we aren't dealing with a multi-byte string.
     if (strlen($string) === mb_strlen($string)) {
-      return wordwrap($string, $width, $break, $cut);
+      return wordwrap($string, $width, "\n", $cut);
     }
-    // We'll use the $cut variable to access the correct regular expression in the following array.
-    settype($cut, "int");
-    // Do not create these strings in each loop.
-    $regExp = [
-      "#^(?:[\x00-\x7F]|[\xC0-\xFF][\x80-\xBF]+){{$width}}#",
-      "#^(?:[\x00-\x7F]|[\xC0-\xFF][\x80-\xBF]+){{$width},}\b#U"
-    ];
-    $words = explode(" ", $string);
-    $string = "";
-    $c = count($words);
-    for ($i = 0; $i < $c; ++$i) {
-      $stringLength = mb_strlen($words[$i]);
-      $cutLength = ceil($stringLength / $width);
-      $newWord = "";
-      for ($j = 0; $j < $cutLength; ++$j) {
-        preg_match($regExp[$cut], $words[$i], $matches);
-        $newString = $matches[0];
-        $newWord .= $newString . $break;
-        $words[$i] = mb_substr($words[$i], mb_strlen($newString));
+    $strlen = mb_strlen($string);
+    $result = "";
+    $lastStart = $lastSpace = 0;
+    for ($i = 0; $i < $strlen; ++$i) {
+      $char = mb_substr($string, $i, 1);
+      if ($char === "\n") {
+        $result .= mb_substr($string, $lastStart, $i - $lastStart + 1);
+        $lastStart = $lastSpace = $i + 1;
+        continue;
       }
-      $string .= " {$newWord}{$words[$i]}";
+      if ($char === " ") {
+        if ($i - $lastStart >= $width) {
+          $result .= mb_substr($string, $lastStart, $i - $lastStart) . "\n";
+          $lastStart = $i + 1;
+        }
+        $lastSpace = $i;
+        continue;
+      }
+      if ($i - $lastStart >= $width && $cut === true && $lastStart >= $lastSpace) {
+        $result .= mb_substr($string, $lastStart, $i - $lastStart) . "\n";
+        $lastStart = $lastSpace = $i;
+        continue;
+      }
+      if ($i - $lastStart >= $width && $lastStart < $lastSpace) {
+        $result .= mb_substr($string, $lastStart, $lastSpace - $lastStart) . "\n";
+        $lastStart = $lastSpace = $lastSpace + 1;
+        continue;
+      }
     }
-    return $string;
+    if ($lastStart !== $i) {
+      $result .= mb_substr($string, $lastStart, $i - $lastStart);
+    }
+    return $result;
   }
 
 }
