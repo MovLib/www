0dfff7171cfb5a2886ce209a126b130d97fd5ec4
Richard Fussenegger <richard@fussenegger.info>
2013-09-11 13:50:11 +0200

    Mail to Email; updated documentation; moved wordwrap

--- src/MovLib/Data/Delayed/Mailer.php
+++ src/MovLib/Data/Delayed/Mailer.php
@@ -18,8 +18,7 @@
 namespace MovLib\Data\Delayed;
 
 use \MovLib\Exception\MailerException;
-use \MovLib\Utility\String;
-use \MovLib\Presentation\Email\AbstractBase as Email;
+use \MovLib\Presentation\Email\AbstractEmail;
 
 /**
  * Delayed mailer system.
@@ -38,11 +37,11 @@ class Mailer {
 
 
   /**
-   * Array used to collect all mails that should be sent.
+   * Array used to collect all emails that should be sent.
    *
    * @var array
    */
-  private static $mails = [];
+  private static $emails = [];
 
 
   // ------------------------------------------------------------------------------------------------------------------- Public Methods
@@ -54,12 +53,12 @@ class Mailer {
    * @todo Digitally sign with DKIM!
    * @global \MovLib\Data\I18n $i18n
    *   Global i18n instance.
-   * @param \MovLib\Presentation\Email\Base $mail
-   *   The mail to send.
+   * @param \MovLib\Presentation\Email\AbstractEmail $mail
+   *   The email to send.
    * @return this
    * @throws MailerException
    */
-  public function send(Email $mail) {
+  public function send(AbstractEmail $mail) {
     global $i18n;
     $messageId = uniqid("movlib");
     mail(
@@ -70,13 +69,13 @@ class Mailer {
         "Content-Type: text/plain; charset=utf-8",
         "Content-Transfer-Encoding: BASE64",
         "",
-        base64_encode(String::wordwrap($mail->getPlain())),
+        base64_encode($this->wordwrap($mail->getPlain())),
         "",
         "--{$messageId}",
         "Content-Type: text/html; charset=utf-8",
         "Content-Transfer-Encoding: BASE64",
         "",
-        base64_encode(String::collapseWhitespace($mail->getHtml())),
+        base64_encode($mail->getHtml()),
         "",
         "--{$messageId}--",
       ]),
@@ -92,30 +91,89 @@ class Mailer {
     return $this;
   }
 
+  /**
+   * Multi-byte aware wordwrap implementation.
+   *
+   * Please note that this function will always normalize line feeds to LF.
+   *
+   * @see wordwrap()
+   * @link https://api.drupal.org/api/drupal/core%21vendor%21zendframework%21zend-stdlib%21Zend%21Stdlib%21StringWrapper%21AbstractStringWrapper.php/function/AbstractStringWrapper%3A%3AwordWrap/8
+   * @param string $string
+   *   The string to wrap.
+   * @param int $width [optional]
+   *   The number of characters at which the string will be wrapped, defaults to <code>75</code>.
+   * @param boolean $cut [optional]
+   *   If set to <code>TRUE</code>, the string is always wrapped at or before the specified width. So if you have a word
+   *   that is larger than the given width, it is broken apart. Defaults to <code>FALSE</code>.
+   * @return string
+   *   The string wrapped at the specified length.
+   */
+  public function wordwrap($string, $width = 75, $cut = false) {
+    // Always remove whitespaces at beginning and end, nobody needs them.
+    $string = trim(self::normalizeLineFeeds($string));
+    // Use native function if we aren't dealing with a multi-byte string.
+    if (strlen($string) === mb_strlen($string)) {
+      return wordwrap($string, $width, "\n", $cut);
+    }
+    $strlen = mb_strlen($string);
+    $result = "";
+    $lastStart = $lastSpace = 0;
+    for ($i = 0; $i < $strlen; ++$i) {
+      $char = mb_substr($string, $i, 1);
+      if ($char === "\n") {
+        $result .= mb_substr($string, $lastStart, $i - $lastStart + 1);
+        $lastStart = $lastSpace = $i + 1;
+        continue;
+      }
+      if ($char === " ") {
+        if ($i - $lastStart >= $width) {
+          $result .= mb_substr($string, $lastStart, $i - $lastStart) . "\n";
+          $lastStart = $i + 1;
+        }
+        $lastSpace = $i;
+        continue;
+      }
+      if ($i - $lastStart >= $width && $cut === true && $lastStart >= $lastSpace) {
+        $result .= mb_substr($string, $lastStart, $i - $lastStart) . "\n";
+        $lastStart = $lastSpace = $i;
+        continue;
+      }
+      if ($i - $lastStart >= $width && $lastStart < $lastSpace) {
+        $result .= mb_substr($string, $lastStart, $lastSpace - $lastStart) . "\n";
+        $lastStart = $lastSpace = $lastSpace + 1;
+        continue;
+      }
+    }
+    if ($lastStart !== $i) {
+      $result .= mb_substr($string, $lastStart, $i - $lastStart);
+    }
+    return $result;
+  }
+
 
   // ------------------------------------------------------------------------------------------------------------------- Public Static Methods
 
 
   /**
-   * Sends all mails in the stack.
+   * Sends all emails in the stack.
    */
   public static function run() {
     $mailer = new Mailer();
-    $c = count(self::$mails);
+    $c = count(self::$emails);
     for ($i = 0; $i < $c; ++$i) {
-      $mailer->send(self::$mails[$i]);
+      $mailer->send(self::$emails[$i]);
     }
   }
 
   /**
-   * Add mail to stack.
+   * Add email to stack.
    *
-   * @param \MovLib\View\Mail\AbstractMail $mail
-   *   The mail that should be sent.
+   * @param \MovLib\Presentation\Email\AbstractEmail $email
+   *   The email that should be sent.
    */
-  public static function stack($mail) {
+  public static function stack($email) {
     delayed_register(__CLASS__, 0);
-    self::$mails[] = $mail;
+    self::$emails[] = $email;
   }
 
 }
