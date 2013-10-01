81252eaaa9879b40919869fbc080c474684fe6fe
Richard Fussenegger <richard@fussenegger.info>
2013-10-02 01:19:01 +0200

    Updates to the user's avatar

--- db/seeds/users.sql
+++ db/seeds/users.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 30, 2013 at 03:20 PM
+-- Generation Time: Oct 01, 2013 at 04:45 PM
 -- Server version: 10.0.4-MariaDB-1~wheezy-log
 -- PHP Version: 5.5.2
 
@@ -26,8 +26,8 @@ SET time_zone = "+00:00";
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `name`, `email`, `password`, `access`, `created`, `login`, `private`, `deactivated`, `time_zone_id`, `edits`, `dyn_profile`, `sex`, `system_language_code`, `avatar_name`, `avatar_changed`, `avatar_type`, `birthday`, `country_id`, `real_name`, `website`, `facebook`, `google_plus`, `twitter`) VALUES
-(1, 'Fleshgrinder', 'richard@fussenegger.info', '$2y$10$N/kvo2/A9vAv.8Mkgb4ky.llucBnDaPi5pdW7HPP2OCHV9yDQyjbG', '2013-09-30 14:11:57', '2013-09-30 14:11:57', '2013-09-30 14:11:57', 0, 0, 'Europe/Vienna', 0, '', 0, 'de', 'fleshgrinder', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `users` (`user_id`, `name`, `email`, `password`, `access`, `created`, `login`, `private`, `deactivated`, `time_zone_id`, `edits`, `dyn_profile`, `sex`, `system_language_code`, `avatar_name`, `avatar_extension`, `avatar_changed`, `birthday`, `country_id`, `real_name`, `website`, `facebook`, `google_plus`, `twitter`) VALUES
+(1, 'Fleshgrinder', 'richard@fussenegger.info', '$2y$10$N/kvo2/A9vAv.8Mkgb4ky.llucBnDaPi5pdW7HPP2OCHV9yDQyjbG', '2013-10-01 16:44:29', '2013-10-01 16:44:29', '2013-10-01 16:44:29', 0, 0, 'Europe/Vienna', 0, '', 0, 'de', 'fleshgrinder', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
