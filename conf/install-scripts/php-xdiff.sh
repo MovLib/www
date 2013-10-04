b4d443d2849c467be8276ab49c6156291bb5de37
Richard Fussenegger <richard@fussenegger.info>
2013-10-04 13:02:03 +0200

    Installed PHP's xdiff extension

---
+++ conf/install-scripts/php-xdiff.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+# ----------------------------------------------------------------------------------------------------------------------
+# This file is part of {@link https://github.com/MovLib MovLib}.
+#
+# Copyright © 2013-present {@link http://movlib.org/ MovLib}.
+#
+# MovLib is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
+# License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# MovLib is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY# without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along with MovLib.
+# If not, see {@link http://www.gnu.org/licenses/ gnu.org/licenses}.
+# ----------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
+# PHP xdiff extension installation script.
+#
+# LINK: http://www.php.net/manual/en/book.xdiff.php
+# LINK: http://pecl.php.net/package/xdiff
+# AUTHOR: Richard Fussenegger <richard@fussenegger.info>
+# COPYRIGHT: © 2013-present, MovLib
+# LICENSE: http://www.gnu.org/licenses/agpl.html AGPL-3.0
+# SINCE: 0.0.1-dev
+# ----------------------------------------------------------------------------------------------------------------------
+
+pecl install xdiff-beta
