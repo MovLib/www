cc0448bb377c2a73b64c4f65f21d939ff6bd1017
Richard Fussenegger <richard@fussenegger.info>
2013-10-12 04:50:12 +0200

    Formatting and updated documentation

--- src/MovLib/Presentation/Partial/Navigation.php
+++ src/MovLib/Presentation/Partial/Navigation.php
@@ -98,34 +98,9 @@ class Navigation extends \MovLib\Presentation\AbstractBase {
   public $hideTitle = true;
 
   /**
-   * Callable that will be called with each menuitem before sending it to
-   * <code>\MovLib\Presentation\AbstractPage::a()</code>.
+   * Callable that will be called with each menuitem before sending it to <code>$this->a()</code>.
    *
-   * Useful if you want to iterate over all menuitems after a navigational object was passed around in the application.
-   * It's important that you actually bind the closure to this object. Calling the closure's magic <code>__invoke</code>
-   * method would make it slow, therefor binding is the preferred way (although requires more writing and the
-   * introduction of an additional variable, in order to call <code>\Closure::bindTo()</code>. The reason for this is
-   * simple, <code>__invoke()</code> is a special magic method to call an object as function and involves a lot of
-   * checks and computation. Whilst binding the closure to this object actually copies the created anonymous function
-   * into this instance as a real regular method.
-   *
-   * <b>Example:</b>
-   * We could solve the <code>\MovLib\Presentation\Page::getBreadcrumb()</code> with a closure as well.
-   * <pre>
-   *  $breadcrumb = new Navigation("foo", $i18n->t("Bar"), []);
-   *  $closure = function (&$menuitem, $index, $count) {
-   *    global $i18n;
-   *    if ($index !== 0 && $index !== ($count - 1) && mb_strlen($menuitem[1]) > 25) {
-   *      $menuitem[2]["title"] = $menuitem[1];
-   *      $menuitem[1] = mb_strimwidth($menuitem[1], 0, 25, $i18n->t("…"));
-   *    }
-   *  };
-   *  $closure->bindTo($breadcrumb);
-   * </pre>
-   * But you might have guessed that the closure makes no sense at this point, because it involves lots of math to
-   * exclude the first and the last element. Instead it's much more intelligent to simply loop over the additional
-   * breadcrumb trails (as it is really implemented). But I hope you get the idea.
-   *
+   * @see \MovLib\Test\Presentation\Partial\Navigation::testToStringClosure()
    * @var null|\Closure
    */
   public $closure;
@@ -162,8 +137,8 @@ class Navigation extends \MovLib\Presentation\AbstractBase {
    *   For a more in-depth explanation have a look at {@see AbstractPage::a()}.
    */
   public function __construct($id, $title, array $menuitems) {
-    $this->id = $id;
-    $this->title = $title;
+    $this->id        = $id;
+    $this->title     = $title;
     $this->menuitems = $menuitems;
   }
 
@@ -187,15 +162,15 @@ class Navigation extends \MovLib\Presentation\AbstractBase {
       if (is_callable($this->closure)) {
         call_user_func_array($this->closure, [ &$this->menuitems[$i], $i, $c ]);
       }
-      $menuitem = $this->a($this->menuitems[$i][0], $this->menuitems[$i][1], $this->menuitems[$i][2]);
+      $menuitem   = $this->a($this->menuitems[$i][0], $this->menuitems[$i][1], $this->menuitems[$i][2]);
       $menuitems .= ($this->unorderedList === true) ? "<li>{$menuitem}</li>" : $menuitem;
     }
     if ($this->unorderedList === true) {
       $menuitems .= "</ul>";
     }
-    $this->attributes["id"] = "{$this->id}-nav";
+    $this->attributes["id"]   = "{$this->id}-nav";
     $this->attributes["role"] = "navigation";
-    $this->hideTitle = ($this->hideTitle === true) ? " class='visuallyhidden'" : "";
+    $this->hideTitle          = ($this->hideTitle === true) ? " class='visuallyhidden'" : "";
     return "<nav{$this->expandTagAttributes($this->attributes)}><h2{$this->hideTitle} id='{$this->id}-nav-title'>{$this->title}</h2><div role='menu'>{$menuitems}</div></nav>";
   }
 
