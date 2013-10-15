8fbdb18392bb630def6049ebaf00be13d8d10e6e
Markus Deutschl <deutschl.markus@gmail.com>
2013-10-15 16:07:39 +0200

    Added tests for AbstractList, Ordered and Unordered. Minor fix in Ordered.

---
+++ tests/MovLib/Test/Presentation/Partial/Lists/UnorderedTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*!
+ * This file is part of {@link https://github.com/MovLib MovLib}.
+ *
+ * Copyright © 2013-present {@link http://movlib.org/ MovLib}.
+ *
+ * MovLib is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * MovLib is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with MovLib.
+ * If not, see {@link http://www.gnu.org/licenses/ gnu.org/licenses}.
+ */
+namespace MovLib\Test\Presentation\Partial\Lists;
+
+use \MovLib\Presentation\Partial\Lists\Unordered;
+
+/**
+ * @coversDefaultClass \MovLib\Presentation\Partial\Lists\Unordered
+ * @author Markus Deutschl <mdeutschl.mmt-m2012@fh-salzburg.ac.at>
+ * @copyright © 2013–present, MovLib
+ * @license http://www.gnu.org/licenses/agpl.html AGPL-3.0
+ * @link http://movlib.org/
+ * @since 0.0.1-dev
+ */
+class UnorderedTest extends \MovLib\Test\TestCase {
+
+  public function testTagProperty() {
+    $unorderedList = new Unordered([]);
+    $this->assertEquals("ul", $this->getProperty($unorderedList, "tag"));
+  }
+
+}
