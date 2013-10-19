4740225e9d2febe8180e17141555b64c44b78c2b
Richard Fussenegger <richard@fussenegger.info>
2013-10-20 00:16:25 +0200

    Also set sticky bits

--- src/MovLib/Console/Command/FixPermissions.php
+++ src/MovLib/Console/Command/FixPermissions.php
@@ -58,8 +58,8 @@ class FixPermissions extends \MovLib\Console\Command\AbstractCommand {
     }
     $this->write("Fixing permissions on all directories and files in '{$directory}' ...");
     $this->exec("chown -R movdev:www-data '{$directory}'", "Could not change ownership!");
-    $this->exec("find '{$directory}' -type d -exec chmod 0770 {} \;", "Could not change directory permissions!");
-    $this->exec("find '{$directory}' -type f -exec chmod 0660 {} \;", "Could not change file permissions!");
+    $this->exec("find '{$directory}' -type d -exec chmod 2770 {} \;", "Could not change directory permissions!");
+    $this->exec("find '{$directory}' -type f -exec chmod 2660 {} \;", "Could not change file permissions!");
     $this->exec("find '{$_SERVER["DOCUMENT_ROOT"]}' -type f -regextype posix-egrep -regex '.*(bin|conf)/.*(\.(php|sh)|phpunit)$' -exec chmod 0770 {} \;", "Could not change executable permissions!");
     $this->write("Permissions Fixed!", self::MESSAGE_TYPE_INFO);
   }
