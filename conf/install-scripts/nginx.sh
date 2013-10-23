f8c57ccf2320118515e242f27b60b924bf216b77
Richard Fussenegger <richard@fussenegger.info>
2013-10-23 09:52:32 +0200

    Fixed cache paths and overhauled the init script again

--- conf/install-scripts/nginx.sh
+++ conf/install-scripts/nginx.sh
@@ -77,8 +77,8 @@ msginfo "Changing to directory: ${SD}${NAME}-${VERSION}"
   --pid-path="/run/nginx.pid" \
   --lock-path="/var/lock/nginx.lock" \
   --error-log-path="/var/log/nginx/error.log" \
-  --http-client-body-temp-path="/var/cache/nginx/body" \
-  --http-fastcgi-temp-path="/var/cache/nginx/fastcgi" \
+  --http-client-body-temp-path="/run/shm/nginx/body" \
+  --http-fastcgi-temp-path="/run/shm/nginx/fastcgi" \
   --http-log-path="/var/log/nginx/access.log" \
   --with-cc-opt="-O2 -m64" \
   --with-ld-opt="-m64" \
--- conf/nginx/init.sh
+++ conf/nginx/init.sh
@@ -1,52 +1,101 @@
 #!/bin/sh
 
 ### BEGIN INIT INFO
-# Provides: nginx
-# Required-Start: $local_fs $remote_fs $network $syslog $named
-# Required-Stop: $local_fs $remote_fs $network $syslog $named
-# Default-Start: 2 3 4 5
-# Default-Stop: 0 1 6
-# Short-Description: starts the nginx web server
-# Description: starts nginx using start-stop-daemon
+# Provides:           nginx
+# Required-Start:     $local_fs $remote_fs $network $syslog $named
+# Required-Stop:      $local_fs $remote_fs $network $syslog $named
+# Default-Start:      2 3 4 5
+# Default-Stop:       0 1 6
+# Short-Description:  nginx LSB init script
+# Description:        nginx Linux Standards Base compliant init script.
 ### END INIT INFO
 
-###
-# AUTHOR: Karl Blessing <http://kbeezie.com/debian-ubuntu-nginx-init-script/>
-# AUTHOR: Richard Fussenegger <richard@fussenegger.info>
-###
+# -----------------------------------------------------------------------------
+# This file is part of MovLib.
+#
+# Copyright © 2013 MovLib.
+#
+# MovLib is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# MovLib is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with MovLib. If not, see <http://www.gnu.org/licenses/>.
+# -----------------------------------------------------------------------------
 
-NAME="nginx"
-DESC="nginx web server"
+# -----------------------------------------------------------------------------
+# nginx Linux Standards Base compliant init script.
+#
+# LINK:       https://wiki.debian.org/LSBInitScripts
+# AUTHOR:     Karl Blessing <http://kbeezie.com/debian-ubuntu-nginx-init-script/>
+# AUTHOR:     Richard Fussenegger <richard@fussenegger.info>
+# COPYRIGHT:  © 2013 MovLib
+# LICENSE:    http://www.gnu.org/licenses/agpl.html AGPL-3.0
+# SINCE:      0.0.1-dev
+# -----------------------------------------------------------------------------
 
-BODY_PATH="/run/shm/${NAME}/body"
-DAEMON="/usr/local/sbin/${NAME}"
+
+# -----------------------------------------------------------------------------
+#                                                                      Includes
+# -----------------------------------------------------------------------------
+
+
+# Load the VERBOSE settings and other rcS variables.
+. /lib/init/vars.sh
+
+# Load the LSB log_* functions.
+. /lib/lsb/init-functions
+
+
+# -----------------------------------------------------------------------------
+#                                                                     Variables
+# -----------------------------------------------------------------------------
+
+
+# The full path to the nginx executable.
+DAEMON="/usr/local/sbin/nginx"
+
+# Arguments that should be passed to nginx.
 DAEMON_ARGS=""
-FASTCGI_PATH="/run/shm/${NAME}/fastcgi/temp"
-PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin"
-PIDFILE="/var/run/${NAME}.pid"
+
+# See compilation flag --http-client-body-temp-path
+PATH_BODY="/run/shm/nginx/body"
+
+# See compilation flag --http-fastcgi-temp-path
+PATH_FCGI="/run/shm/nginx/fastcgi/temp"
+
+# Absolute path to the PID file.
+PIDFILE="/var/run/nginx.pid"
+
+
+# -----------------------------------------------------------------------------
+#                                                                     Bootstrap
+# -----------------------------------------------------------------------------
+
 
 # Check return status of EVERY command
 set -e
 
-# Load the VERBOSE setting and other rcS variables
-. /lib/init/vars.sh
-
-# Define LSB log_* functions.
-# Depend on lsb-base (>= 3.0-6) to ensure that this file is present.
-. /lib/lsb/init-functions
-
+# Check if nginx is a file and executable, if not assume it's not installed.
 if [ ! -x ${DAEMON} ]; then
-  log_failure_msg "${NAME} not installed"
+  log_failure_msg "nginx not installed"
   exit 1
 fi
 
+# This script is only accessible for root (sudo).
 if [ $(id -u) != 0 ]; then
   log_failure_msg "super user only!"
   exit 1
 fi
 
 # Create cache directories if the don't exist
-test -d ${BODY_PATH} || mkdir -p ${BODY_PATH}
+test -d ${PATH_BODY} || mkdir -p ${PATH_BODY}
 test -d ${FCGI_PATH} || mkdir -p ${FCGI_PATH}
 
 # Always validate configuration, display problem if any and exit script.
@@ -54,6 +103,12 @@ ${DAEMON} -qt
 
 # Always check if nginx is already running (used later for proper error messages).
 RUNNING=$(start-stop-daemon --start --quiet --pidfile ${PIDFILE} --exec ${DAEMON} --test && echo "false" || echo "true")
+
+
+# -----------------------------------------------------------------------------
+#                                                                     Functions
+# -----------------------------------------------------------------------------
+
 
 ###
 # Starts the nginx service.
@@ -74,22 +129,29 @@ start_nginx() {
 #   1 - stopping failed
 ###
 stop_nginx() {
-  start-stop-daemon --stop --quiet --pidfile ${PIDFILE} --name ${NAME}
+  start-stop-daemon --stop --quiet --pidfile ${PIDFILE} --name nginx
 }
 
 ###
-# Load OCSP file into nginx's cache.
+# Load OCSP file into nginx's cache. Executed as a detached process as it may take some time.
 ###
 load_ocsp_file() {
   openssl s_client -connect 127.0.0.1:443 -tls1 -tlsextdebug -status <&- 1<&- 2<&- &
 }
 
+
+# -----------------------------------------------------------------------------
+#                                                                  Handle Input
+# -----------------------------------------------------------------------------
+
+
 case ${1} in
+
   start)
     if [ ${RUNNING} = "true" ]; then
-      log_daemon_msg "starting ${NAME}" "already running"
+      log_daemon_msg "starting nginx" "already running"
     else
-      log_daemon_msg "starting ${NAME}"
+      log_daemon_msg "starting nginx"
       start_nginx || log_end_msg 1
     fi
     load_ocsp_file
@@ -98,9 +160,9 @@ case ${1} in
 
   stop)
     if [ ${RUNNING} = "false" ]; then
-      log_daemon_msg "stopping ${NAME}" "not running"
+      log_daemon_msg "stopping nginx" "not running"
     else
-      log_daemon_msg "stopping ${NAME}"
+      log_daemon_msg "stopping nginx"
       stop_nginx || log_end_msg 1
     fi
     load_ocsp_file
@@ -109,10 +171,10 @@ case ${1} in
 
   restart)
     if [ ${RUNNING} = "false" ]; then
-      log_daemon_msg "restarting ${NAME}" "not running"
+      log_daemon_msg "restarting nginx" "not running"
       log_end_msg 1
     else
-      log_daemon_msg "restarting ${NAME}"
+      log_daemon_msg "restarting nginx"
       stop_nginx || log_end_msg 1
       sleep 1
       start_nginx || log_end_msg 1
@@ -121,12 +183,12 @@ case ${1} in
     log_end_msg 0
   ;;
 
-  reload)
+  reload|force-reload)
     if [ ${RUNNING} = "false" ]; then
-      log_daemon_msg "reloading ${NAME} configuration" "not running"
+      log_daemon_msg "reloading nginx configuration" "not running"
       log_end_msg 1
     else
-      log_daemon_msg "reloading ${NAME} configuration"
+      log_daemon_msg "reloading nginx configuration"
       start-stop-daemon --stop --signal HUP --quiet --pidfile ${PIDFILE} --exec ${DAEMON} || log_end_msg 1
     fi
     load_ocsp_file
@@ -134,11 +196,11 @@ case ${1} in
   ;;
 
   status)
-    status_of_proc "${DAEMON}" "${NAME}" && exit 0 || exit 1
+    status_of_proc "${DAEMON}" "nginx" && exit 0 || exit 1
   ;;
 
   *)
-    echo "Usage: ${SCRIPTNAME} (start|stop|restart|reload|status}" >&2
+    echo "Usage: ${SCRIPTNAME} (start|stop|restart|reload|force-reload|status}" >&2
     exit 1
   ;;
 
