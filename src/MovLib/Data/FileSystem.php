dfb77ea782f3dffbb452a692350303412a4f0ac1
Richard Fussenegger <richard@fussenegger.info>
2013-10-31 13:34:00 +0100

    Move filename to static class

---
+++ src/MovLib/Data/FileSystem.php
@@ -0,0 +1,47 @@
+<?php
+
+/*!
+ * This file is part of {@link https://github.com/MovLib MovLib}.
+ *
+ * Copyright © 2013-present {@link https://movlib.org/ MovLib}.
+ *
+ * MovLib is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * MovLib is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with MovLib.
+ * If not, see {@link http://www.gnu.org/licenses/ gnu.org/licenses}.
+ */
+namespace MovLib\Data;
+
+/**
+ * Provides various static methods related to file system actions.
+ *
+ * @author Richard Fussenegger <richard@fussenegger.info>
+ * @copyright © 2013 MovLib
+ * @license http://www.gnu.org/licenses/agpl.html AGPL-3.0
+ * @link https://movlib.org/
+ * @since 0.0.1-dev
+ */
+class FileSystem {
+
+  /**
+   * Sanitizes a filename, replacing whitespace with dashes and transforming the string to lowercase.
+   *
+   * Removes special characters that are illegal in filenames on certain operating systems and special characters
+   * requiring special escaping to manipulate at the command line. Replaces spaces and consecutive dashes with a single
+   * dash. Trims period, dash und underscore from beginning and end of filename.
+   *
+   * @param string $filename
+   *   The filename to be sanitized.
+   * @return string
+   *   The sanitized filename.
+   */
+  public static function sanitizeFilename($filename) {
+    return mb_strtolower(trim(preg_replace("/[\s-]+/", "-", str_replace([ "?", "[", "]", "/", "\\", "=", "<", ">", ":", ";", ",", "'", '"', "&", "$", "#", "*", "(", ")", "|", "~" ], "", $filename)), ".-_"));
+  }
+
+}
