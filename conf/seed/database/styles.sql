6997e94e975eb733229d0afc8db71d67feb85cac
Richard Fussenegger <richard@fussenegger.info>
2013-11-25 14:50:41 +0100

    Overhauled genres and styles

--- conf/seed/database/styles.sql
+++ conf/seed/database/styles.sql
@@ -1,45 +1,42 @@
--- phpMyAdmin SQL Dump
--- version 4.0.8
--- http://www.phpmyadmin.net
+-- ---------------------------------------------------------------------------------------------------------------------
+-- This file is part of {@link https://github.com/MovLib MovLib}.
 --
--- Host: localhost
--- Generation Time: Oct 11, 2013 at 01:27 PM
--- Server version: 10.0.4-MariaDB-1~wheezy-log
--- PHP Version: 5.5.4
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
+-- Copyright © 2013-present {@link https://movlib.org/ MovLib}.
 --
--- Database: `movlib`
+-- MovLib is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
+-- License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+-- version.
 --
-
+-- MovLib is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+-- of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
 --
--- Dumping data for table `styles`
+-- You should have received a copy of the GNU Affero General Public License along with MovLib.
+-- If not, see {@link http://www.gnu.org/licenses/ gnu.org/licenses}.
+-- ---------------------------------------------------------------------------------------------------------------------
+
+-- ---------------------------------------------------------------------------------------------------------------------
+-- Styles seed data.
 --
+-- @author Richard Fussenegger <richard@fussenegger.info>
+-- @copyright © 2013 MovLib
+-- @license http://www.gnu.org/licenses/agpl.html AGPL-3.0
+-- @link https://movlib.org/
+-- @since 0.0.1-dev
+-- ---------------------------------------------------------------------------------------------------------------------
 
-INSERT INTO `styles` (`style_id`, `name`, `description`, `dyn_names`, `dyn_descriptions`) VALUES
-(1, 'Film noir', '', '', ''),
-(2, 'Color film noir', '', '', ''),
-(3, 'Neo-noir', '', '', ''),
-(4, 'Cinema verite', '', '', ''),
-(5, 'Direct Cinema', '', '', ''),
-(6, 'Documentary mode', '', '', ''),
-(7, 'Fly on the wall', '', '', ''),
-(8, 'Tex Murphy', '', '', ''),
-(9, 'Ghost in the Shell films', '', '', ''),
-(10, 'Terminator films', '', '', ''),
-(11, '3D Animation', '', '\0\0\0\0\0de!3D Animation', '');
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO `styles` (`dyn_names`, `dyn_descriptions`) VALUES
+(
+  COLUMN_CREATE('en', 'Heroic Bloodshed', 'de', 'Heroic Bloodshed'),
+  COLUMN_CREATE(
+    'en', '&lt;p&gt;&lt;strong&gt;Heroic Bloodshed&lt;/strong&gt; is a style of the &lt;a href="/genre/1"&gt;action film genre&lt;/a&gt;.&lt;/p&gt;',
+    'de', '&lt;p&gt;&lt;strong&gt;Heroic Bloodshed&lt;/strong&gt; ist ein Stil des &lt;a href="/genre/1"&gt;Actionfilm Genres&lt;/a&gt;.&lt;/p&gt;'
+  )
+),
+(
+  COLUMN_CREATE('en', 'Martial Arts', 'de', 'Martial-Arts'),
+  COLUMN_CREATE(
+    'en', '&lt;p&gt;&lt;strong&gt;Marial arts&lt;/strong&gt; is a style of the &lt;a href="/genre/1"&gt;action film genres&lt;/a&gt;.&lt;/p&gt;',
+    'de', '&lt;p&gt;&lt;strong&gt;Marial Arts&lt;/strong&gt; ist ein Stil des &lt;a href="/genre/1"&gt;Actionfilm Genres&lt;/a&gt;.&lt;/p&gt;'
+  )
+)
+;
