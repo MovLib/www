6ec70c1a2008e22ae8b54e634f76642732657d8a
Markus Deutschl <deutschl.markus@gmail.com>
2013-12-16 10:53:36 +0100

    Finished presentation of the system pages.

---
+++ src/MovLib/Presentation/SystemPage/Show.php
@@ -0,0 +1,58 @@
+<?php
+
+/* !
+ * This file is part of {@link https://github.com/MovLib MovLib}.
+ *
+ * Copyright © 2013-present {@link https://movlib.org/ MovLib}.
+ *
+ * MovLib is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * MovLib is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with MovLib.
+ * If not, see {@link http://www.gnu.org/licenses/ gnu.org/licenses}.
+ */
+namespace MovLib\Presentation\SystemPage;
+
+/**
+ * Single system page presentation.
+ *
+ * @author Markus Deutschl <mdeutschl.mmt-m2012@fh-salzburg.ac.at>
+ * @copyright © 2013 MovLib
+ * @license http://www.gnu.org/licenses/agpl.html AGPL-3.0
+ * @link https://movlib.org/
+ * @since 0.0.1-dev
+ */
+class Show extends \MovLib\Presentation\Page {
+
+  /**
+   * The system page to present.
+   *
+   * @var \MovLib\Data\SystemPage
+   */
+  protected $systemPage;
+
+  /**
+   * * Instantiate new system page presentation.
+   *
+   * @global \MovLib\Data\I18n $i18n
+   * @global \MovLib\Kernel $kernel
+   * @global \MovLib\Data\User\Session $session
+   */
+  public function __construct() {
+    // Instantiate the data model.
+    $this->systemPage = new \MovLib\Data\SystemPage($_SERVER["ID"]);
+    $this->init($this->systemPage->title);
+  }
+
+  /**
+   * @inheritdoc
+   */
+  protected function getContent() {
+    return "<div class='c'><div class='r'><div class='s s12'>{$this->systemPage->text}</div></div></div>";
+  }
+
+}
