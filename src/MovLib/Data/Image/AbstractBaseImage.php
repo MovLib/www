339889773b1248cfb7c4f4a45a78ed91f46ad8de
Richard Fussenegger <richard@fussenegger.info>
2014-01-19 22:33:05 +0100

    Changed convert parameters for better results

--- src/MovLib/Data/Image/AbstractBaseImage.php
+++ src/MovLib/Data/Image/AbstractBaseImage.php
@@ -264,7 +264,7 @@ abstract class AbstractBaseImage {
 
     // Generate the desired image style with ImageMagick. We directly call the binary instead of using some kind of
     // abstraction layer, we don't need any fancy object just to resize an image.
-    if (sh::execute("convert '{$source}' -define 'filter:support=2.5' -filter 'Lagrange' -quality 80 -unsharp '0x0.75+0.75+0.008' -resize {$resizeArg} '{$destination}'", $output) === false) {
+    if (sh::execute("convert '{$source}' -colorspace RGB -filter 'Cosine' -quality 75 -resize {$resizeArg} -colorspace sRGB '{$destination}'", $output) === false) {
       // No need to check for this exception, this only happens if the file system is full or ImageMagick is missing
       // on the server. Both situations are so terrible that we don't need to cover these situations.
       // @codeCoverageIgnoreStart
