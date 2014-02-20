c7dae659f36872c79476427f06b253851a2d504c
Richard Fussenegger <richard@fussenegger.info>
2014-02-20 12:24:58 +0100

    Puppet installer script

---
+++ conf/puppet/puppet-installer.sh
@@ -0,0 +1,32 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# This file is part of {@link https://github.com/MovLib MovLib}.
+#
+# Copyright © 2013-present {@link https://movlib.org/ MovLib}.
+#
+# MovLib is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
+# License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# MovLib is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY# without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along with MovLib.
+# If not, see {@link http://www.gnu.org/licenses/ gnu.org/licenses}.
+# ----------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Puppet installer.
+#
+# LINK:       http://www.vagrantup.com/
+# AUTHOR:     Richard Fussenegger <richard@fussenegger.info>
+# COPYRIGHT:  © 2013 MovLib
+# LICENSE:    http://www.gnu.org/licenses/agpl.html AGPL-3.0
+# LINK:       https://movlib.org/
+# SINCE:      0.0.1-dev
+# ----------------------------------------------------------------------------------------------------------------------
+
+wget -O /tmp/puppetlabs-release-wheezy.deb http://apt.puppetlabs.com/puppetlabs-release-wheezy.deb
+dpkg -i /tmp/puppetlabs-release-wheezy.deb
+rm -f /tmp/puppetlabs-release-wheezy.deb
+aptitude update
+aptitude -y install puppet facter
