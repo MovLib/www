b03d4cc4641b43f6f7bce9bd545abe9b72514111
Markus Deutschl <deutschl.markus@gmail.com>
2014-03-05 16:11:39 +0100

    Added Trait for Gone presentations.

---
+++ src/MovLib/Presentation/TraitGone.php
@@ -0,0 +1,64 @@
+<?php
+
+/* !
+ * This file is part of {@link https://github.com/MovLib MovLib}.
+ *
+ * Copyright © 2013-present {@link https://movlib.org/ MovLib}.
+ *
+ * MovLib is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * MovLib is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with MovLib.
+ * If not, see {@link http://www.gnu.org/licenses/ gnu.org/licenses}.
+ */
+namespace MovLib\Presentation;
+
+use \MovLib\Presentation\Partial\Alert;
+
+/**
+ * @todo Description of TraitGone
+ *
+ * @author Markus Deutschl <mdeutschl.mmt-m2012@fh-salzburg.ac.at>
+ * @copyright © 2013 MovLib
+ * @license http://www.gnu.org/licenses/agpl.html AGPL-3.0
+ * @link https://movlib.org/
+ * @since 0.0.1-dev
+ */
+trait TraitGone {
+
+  /**
+   * The translated gone alert message.
+   *
+   * @var string
+   */
+  protected $goneAlertMessage;
+
+  /**
+   * Get the content for gone pages.
+   *
+   * @global \MovLib\Data\I18n $i18n
+   * @return \MovLib\Presentation\Partial\Alert
+   *   The gone alert message.
+   */
+  protected function goneGetContent() {
+    global $i18n;
+    // @devStart
+    // @codeCoverageIgnoreStart
+    if (empty($this->goneAlertMessage)) {
+      throw new \LogicException($i18n->t("You have to provide a message for the gone page!"));
+    }
+    // @codeCoverageIgnoreEnd
+    // @devEnd
+
+    return new Alert(
+      "<p>{$i18n->t("The deletion message is provided below for reference.")}</p><p>{$this->goneAlertMessage}</p>",
+      $i18n->t("This page has been deleted."),
+      Alert::SEVERITY_ERROR
+    );
+  }
+
+}
