dd7bc820352d63b2ed7b0a668b72fc1e9f73f438
Franz Torghele <franz@torghele.at>
2014-03-14 13:05:21 +0100

    awards seed: added academy award description

--- conf/seed/database/awards.sql
+++ conf/seed/database/awards.sql
@@ -42,15 +42,15 @@ INSERT INTO `places` SET
 INSERT INTO `awards` SET
   `created`                = CURRENT_TIMESTAMP,
   `dyn_descriptions`       = COLUMN_CREATE(
-    'en', '',
-    'de', ''
+    'en', '&lt;p&gt;The Academy Awards, commonly known as The Oscars, is an annual American awards ceremony honoring achievements in the film industry. Winners are awarded the statuette, officially the Academy Award of Merit, that is much better known by its nickname Oscar. The awards, first presented in 1929 at the Hollywood Roosevelt Hotel, are overseen by the Academy of Motion Picture Arts and Sciences (AMPAS).&lt;/p&gt;&lt;p&gt;The awards ceremony was first televised in 1953 and is now seen live in more than 200 countries. The Oscars is also the oldest entertainment awards ceremony; its equivalents, the Emmy Awards for television, the Tony Awards for theatre, and the Grammy Awards for music and recording, are modeled after the Academy Awards.&lt;/p&gt;&lt;p&gt;The 86th Academy Awards were held on March 2, 2014, at the Dolby Theatre in Los Angeles.&lt;/p&gt;',
+    'de', '&lt;p&gt;Der Academy Award, besser bekannt unter seinem Spitznamen Oscar, ist ein Filmpreis. Er wird jährlich von der US-amerikanischen Academy of Motion Picture Arts and Sciences (AMPAS) für die besten Filme des Vorjahres verliehen, wobei wegen der Zulassungsprozedur in der Regel US-Produktionen dominieren. Die letzte Verleihung fand am 2. März 2014 in Los Angeles statt.&lt;/p&gt;&lt;p&gt;Die Auszeichnung wurde am 12. Februar 1929 vom damaligen Präsidenten der MGM Studios, Louis B. Mayer, ins Leben gerufen, fast neun Jahre nach der Verleihung des Photoplay Awards, der als erster Filmpreis der Welt gilt. Der Oscar wird jährlich in einer gemeinsamen Zeremonie in derzeit über 30 verschiedenen Kategorien in Form jeweils einer Statuette vergeben, die einen Ritter mit einem Schwert auf einer Filmrolle darstellt.&lt;/p&gt;&lt;p&gt;In die Auswahl zur Verleihung eines oder auch mehrerer Oscars kommen hauptsächlich amerikanische Spielfilme. In jeweils eigenen Kategorien werden Kurz-, Dokumentar-, Animations- und ausländische Filme prämiert. Für die Qualifikation eines amerikanischen Spielfilms zur Auswahl gilt die Bedingung, dass er im Vorjahr der Verleihung mindestens sieben Tage lang in einem öffentlichen Kino im Gebiet von Los Angeles County – dem Heimatbezirk von Hollywood – gegen Entgelt gezeigt wurde.&lt;/p&gt;'
   ),
   `dyn_wikipedia`          = COLUMN_CREATE(
-    'en', 'http://en.wikipedia.org/wiki/Oscar',
+    'en', 'http://en.wikipedia.org/wiki/Academy_Award',
     'de', 'http://de.wikipedia.org/wiki/Oscar'
   ),
   `dyn_names`          = COLUMN_CREATE(
-    'en', 'Oscar',
+    'en', 'Academy Awards',
     'de', 'Oscar'
   ),
   `links`                  = 'a:1:{i:0;s:22:"http://www.oscars.org/";}',
