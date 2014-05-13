74d44eea25ca03383354c4f2b705a2b34115aed9
Franz Torghele <franz@torghele.at>
2014-05-13 16:48:43 +0200

    added user related count command.

---
+++ src/MovLib/Console/Command/Install/Count/UserCount.php
@@ -0,0 +1,51 @@
+<?php
+
+/*!
+ * This file is part of {@link https://github.com/MovLib MovLib}.
+ *
+ * Copyright © 2013-present {@link https://movlib.org/ MovLib}.
+ *
+ * MovLib is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
+ * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * MovLib is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with MovLib.
+ * If not, see {@link http://www.gnu.org/licenses/ gnu.org/licenses}.
+ */
+namespace MovLib\Console\Command\Install\Count;
+
+/**
+ * Count verification for users.
+ *
+ * @author Franz Torghele <ftorghele.mmt-m2012@fh-salzburg.ac.at>
+ * @copyright © 2014 MovLib
+ * @license http://www.gnu.org/licenses/agpl.html AGPL-3.0
+ * @link https://movlib.org/
+ * @since 0.0.1-dev
+ */
+class UserCount extends \MovLib\Console\Command\Install\Count\AbstractEntityCountCommand {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function configure() {
+    $this->entityName = "User";
+    $this->tableName = "users";
+    $this->addCountColumn("lists", [ $this, "getCounts" ], [ "user_id", null, "id", "users_lists" ]);
+    $this->addCountColumn(
+      "contributions",
+      [ $this, "getCounts" ],
+      [ "user_id", null, [ "entity_type_id", "entity_id", "created" ], "revisions"]
+    );
+    $this->addCountColumn(
+      "uploads",
+      [ $this, "getCounts" ],
+      [ "user_id", null, [ "entity_type_id", "entity_id", "created" ], "image_revisions", "`path` IS NOT NULL"]
+    );
+    return parent::configure();
+  }
+
+}
