0b4cbacbcbacdf355fc246ebd930608baf76d717
Richard Fussenegger <richard@fussenegger.info>
2014-05-15 17:41:05 +0200

    Translated the complete home page

--- src/MovLib/Presentation/Home.php
+++ src/MovLib/Presentation/Home.php
@@ -43,6 +43,7 @@ final class Home extends \MovLib\Presentation\AbstractPresenter {
    * {@inheritdoc}
    */
   public function getContent() {
+    $tSitenameArg = [ "sitename" => $this->config->sitename ];
     $articles = [];
 
     $articles[$this->intl->t("Movies")] = "<p>{$this->intl->t(
@@ -63,30 +64,41 @@ final class Home extends \MovLib\Presentation\AbstractPresenter {
     )}</p>";
 
     $articles[$this->intl->t("Releases")] = "<p>{$this->intl->t(
-      "Insert text here …"
+      "The exact release database of {sitename} allows you to search for specific releases, check if a release is " .
+      "truly uncut, or simply lookup some technical specs of a release. It allows you to keep track of the exact " .
+      "releases you own, or the ones you’d like to buy. Cinema, DVD, TV, Digital Download, … doesn’t matter, all " .
+      "forms and formats have a place at {sitename}.",
+      $tSitenameArg
     )}</p>";
 
-    $articles[$this->intl->t("My {sitename}", [ "sitename" => $this->config->sitename ])] = "<p>{$this->intl->t(
-      "Insert text here …"
-    )}</p><p><a class='btn btn-success btn-large' href='{$this->intl->r("/profile/join")}'>{$this->intl->t(
-      "Join {sitename}", [ "sitename" => $this->config->sitename ])
-    }</a></p>";
+    $articles[$this->intl->t("My {sitename}", $tSitenameArg)] = "<p>{$this->intl->t(
+      "If you join {sitename} you’ll be able to use the extensive statistical tools that we provide to you for free. " .
+      "You can easily keep track of all the releases you own, the ones you want to buy, create lists (like “All " .
+      "movies with cats as main actors”), track your contributions, rate movies and series, and much more …",
+      $tSitenameArg
+    )}</p>";
 
     $articles["<abbr title='{$this->intl->t("Application Programming Interface")}'>{$this->intl->t("API")}</abbr>"] = "<p>{$this->intl->t(
       "The {sitename} API is a REST interface to access the free movie library. Specifically designed for all " .
       "developers out there. We want to keep the barrier as low as possible and ensure that everybody can use the " .
       "data we all collect here at {sitename}.",
-      [ "sitename" => $this->config->sitename ]
-    )}</p><p><a class='btn btn-info btn-large' href='{$this->intl->r("/help/api")}'>{$this->intl->t(
-      "Read the API documentation"
-    )}</a></p>";
+      $tSitenameArg
+    )}</p>";
 
-    $content = "";
+    $content = null;
     foreach ($articles as $title => $body) {
-      $content .= "<article class='s s4 taj'><h2 class='tac'>{$title}</h2>{$body}</article>";
+      $content .= "<article class='s s4'><h2 class='tac'>{$title}</h2>{$body}</article>";
     }
 
-    return "<div class='c'><div class='r'>{$content}</div></div>";
+    return
+      "<div class='c'><div class='r'>{$content}<div class='s s4 tac'>{$this->a(
+        "/releases/create", $this->intl->t("Add a Release"), [ "class" => "btn btn-info btn-large" ]
+      )}</div><div class='s s4 tac'>{$this->a(
+        "/profile/join", $this->intl->t("Join {sitename}", $tSitenameArg), [ "class" => "btn btn-success btn-large" ]
+      )}</div><div class='s s4 tac'>{$this->a(
+        "/help/api", $this->intl->t("Read the API documentation"),  [ "class" => "btn btn-large" ]
+      )}</div></div></div>"
+    ;
   }
 
   /**
