e2980b5b22df46a207d216c904520a7d5d911920
Franz Torghele <franz@torghele.at>
2014-06-03 16:22:00 +0200

    removed dead link

--- src/MovLib/Partial/StarRatingForm.php
+++ src/MovLib/Partial/StarRatingForm.php
@@ -141,8 +141,8 @@ final class StarRatingForm extends \MovLib\Core\Presentation\DependencyInjection
         "Rated by {0,plural,=1{{ratingVotes} user} other{{ratingVotes} users}} with {0,plural,=1{{rating}} other{a {1}mean rating{2} of {rating}}}.",
         [
           $entity->ratingVotes,
-          "<a href='{$entity->r("/ratings")}' title='{$this->intl->t("View the rating demographics.")}'>",
-          "</a>",
+          "<span>",  // @todo: change back to "<a href='{$entity->r("/ratings")}' title='{$this->intl->t("View the rating demographics.")}'>",
+          "</span>", //                       "</a>",
           "ratingVotes"  => "<span property='ratingCount'>{$this->intl->format("{0,number}", $entity->ratingVotes)}</span>",
           "rating" => "<span property='ratingValue'>{$this->intl->format("{0,number}", $entity->ratingMean)}</span>",
         ]
